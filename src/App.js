22cfa3fdd816b6f6d533a0045b5a5afbf2663514
hj-66 <a01088200534@gmail.com>
2025-10-22 08:11:23 +0900

    feat(getCarsInput): 자동차 이름 입력 후 구분

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils'
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await this.getCarsInput();
+  }
+
+  async getCarsInput() {
+    const cars = await Console.readLineAsync('');
+    const splitCars = cars.split(',');
+    return splitCars;
+  }
 }
 
 export default App;
