efba72cca806b65d4e721aadd8c9073b10794645
hj-66 <a01088200534@gmail.com>
2025-10-22 09:05:07 +0900

    feat(getNumberInput): 시도 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Console } from '@woowacourse/mission-utils'
 class App {
   async run() {
     const cars = await this.getCarsInput();
+    const numberAttempts = await this.getNumberInput();
   }
 
   async getCarsInput() {
     const cars = await Console.readLineAsync('');
     const splitCars = cars.split(',');
     return splitCars;
+  }
+
+  async getNumberInput() {
+    const numberAttempts = await Console.readLineAsync('');
+    return numberAttempts;
   }
 }
 
