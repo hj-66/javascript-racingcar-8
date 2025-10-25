b87da8500891a2c38352af592335a6bdf4c162f8
hj-66 <a01088200534@gmail.com>
2025-10-25 14:46:00 +0900

    feat(getNumberInput): 시도 횟수 에러 검출 기능 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { Console, MissionUtils } from '@woowacourse/mission-utils';
-import { MESSAGES } from './constants.js';
-import { validateAllowedCharacter, validateNameLength } from './validation.js';
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGES } from "./constants.js";
+import {
+  validateAllowedCharacter,
+  validateNameLength,
+  validateNumberAttempts,
+} from "./validation.js";
 
 class App {
   async run() {
@@ -16,7 +20,7 @@ class App {
 
   async getCarsInput() {
     const cars = await Console.readLineAsync(MESSAGES.INPUT.INPUT_CARS);
-    const splitCars = cars.split(',');
+    const splitCars = cars.split(",");
     let executionResult = [];
     splitCars.forEach((splitCar) => {
       executionResult.push(0);
@@ -27,37 +31,42 @@ class App {
   }
 
   async getNumberInput() {
-    const numberAttempts = await Console.readLineAsync(MESSAGES.INPUT.INPUT_ATTEMPTS);
+    const input = await Console.readLineAsync(MESSAGES.INPUT.INPUT_ATTEMPTS);
+    const numberAttempts = Number(input);
+    validateNumberAttempts(numberAttempts);
     return numberAttempts;
   }
 
   runRace(cars, executionResult, numberAttempts) {
-    Console.print('');
+    Console.print("");
     Console.print(MESSAGES.OUTPUT.OUTPUT_RACE);
     for (let i = 0; i < numberAttempts; i++) {
       cars.forEach((car, index) => {
         const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
         if (randomNumber >= 4) executionResult[index] += 1;
-      })
+      });
       this.printRace(cars, executionResult);
     }
   }
 
   printRace(cars, executionResult) {
     cars.forEach((car, index) => {
-      Console.print(`${car} : ${'-'.repeat(executionResult[index])}`);
-    })
-    Console.print('');
+      Console.print(`${car} : ${"-".repeat(executionResult[index])}`);
+    });
+    Console.print("");
   }
 
   printResult(cars, executionResult) {
     let maxNumber = 0;
     let winners = [];
     executionResult.forEach((result, index) => {
-      if (result > maxNumber) { winners = []; maxNumber = result; }
+      if (result > maxNumber) {
+        winners = [];
+        maxNumber = result;
+      }
       if (result >= maxNumber) winners.push(cars[index]);
-    })
-    Console.print(MESSAGES.OUTPUT.OUTPUT_WINNER + winners.join(','));
+    });
+    Console.print(MESSAGES.OUTPUT.OUTPUT_WINNER + winners.join(","));
   }
 }
 
