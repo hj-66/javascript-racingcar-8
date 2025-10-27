407be7ed9b8cf6b3e03d65df40ea07c67132c29c
hj-66 <a01088200534@gmail.com>
2025-10-27 14:38:32 +0900

    feat(Car): App.js기반 Car.js 클래스 기능 작성

---
+++ src/model/Car.js
@@ -0,0 +1,14 @@
+export default class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move(randomNumber) {
+    if (randomNumber >= 4) this.position += 1;
+  }
+
+  getTrack() {
+    return '-'.repeat(this.position);
+  }
+}
