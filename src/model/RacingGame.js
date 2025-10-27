b999cb2817cc005142f832f15602d68ca2e73018
hj-66 <a01088200534@gmail.com>
2025-10-27 14:41:21 +0900

    feat(RacingGame): App.js 기반 RacingGame 클래스 기능 작성

---
+++ src/model/RacingGame.js
@@ -0,0 +1,22 @@
+import Car from "./Car.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export default class RacingGame {
+  constructor(carNames) {
+    this.cars = carNames.map((name) => new Car(name));
+  }
+
+  raceOnce() {
+    this.cars.forEach((car) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      car.move(randomNumber);
+    });
+  }
+
+  getWinners() {
+    const max = Math.max(...this.cars.map((car) => car.position));
+    return this.cars
+      .filter((car) => car.position === max)
+      .map((car) => car.name);
+  }
+}
